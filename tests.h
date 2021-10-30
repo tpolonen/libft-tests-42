75570e16995550c1ebc0aaeb60a2b76786b093d1
tpolonen <tpolonen@student.hive.fi>
2021-10-30 19:10:24 +0300

    added the first test

--- tests.h
+++ tests.h
@@ -1,36 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tests.c                                            :+:      :+:    :+:   */
+/*   tests.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/29 18:36:42 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/10/30 18:44:00 by tpolonen         ###   ########.fr       */
+/*   Created: 2021/10/30 18:49:43 by tpolonen          #+#    #+#             */
+/*   Updated: 2021/10/30 18:50:28 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-#include <assert.h>
-#include <stdlib.h>
+#ifndef TESTS_H
+# define TESTS_H
 
-// TODO: replace free with libft-equivalent
-void	test_memalloc(void)
-{
-	void	*ptr;
-	char	*str;
-	int		i;
+void	test_memalloc(void);
 
-	ptr = ft_memalloc(sizeof(char) * 8);
-	str = (char *) ptr;
-	i = 0;
-	while (i < 7)
-	{
-		str[i] = 'a';
-		i++;
-	}
-	str[i] = '\0';
-	assert(ft_strcmp("aaaaaaa", (char *) ptr) == 0);
-	free(ptr);
-	str = 0;
-}
+#endif
