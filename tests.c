67348ba44edf4353112bfbc294a3c53165182c27
tpolonen <merirosvo@kyy.ninja>
2021-10-30 20:07:16 +0300

    test added

--- tests.c
+++ tests.c
@@ -6,15 +6,15 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 18:36:42 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/10/30 18:44:00 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/10/30 19:56:16 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <assert.h>
 #include <stdlib.h>
+#include <string.h>
 
-// TODO: replace free with libft-equivalent
 void	test_memalloc(void)
 {
 	void	*ptr;
@@ -30,7 +30,7 @@ void	test_memalloc(void)
 		i++;
 	}
 	str[i] = '\0';
-	assert(ft_strcmp("aaaaaaa", (char *) ptr) == 0);
+	assert(strcmp("aaaaaaa", (char *) ptr) == 0);
 	free(ptr);
 	str = 0;
 }
