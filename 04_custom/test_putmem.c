87bcbd2dc3802f6c34ab81882074f2883106f7b4
tpolonen <tpolonen@student.hive.fi>
2021-12-08 20:33:34 +0200

    first custom function added

--- 04_custom/test_putmem.c
+++ 04_custom/test_putmem.c
@@ -1,36 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_putstr.c                                      :+:      :+:    :+:   */
+/*   test_putmem.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/12/01 19:07:34 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/12/08 20:32:59 by tpolonen         ###   ########.fr       */
+/*   Created: 2021/12/08 20:13:08 by tpolonen          #+#    #+#             */
+/*   Updated: 2021/12/08 20:30:52 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "tests.h"
 
-void	test_putstr(void)
+void	test_putmem(void)
 {
-	FILE	*fd;
-	char	*buf;
-	char	str[] = "The ships hung in the sky in much the same way that bricks don't.";
+	FILE	*file;
+	char	str[42];
+	char	buf[42];
 
-	printf("...ft_putstr\n");
-	buf = (char *) malloc(sizeof(char) * (strlen(str) + 1));
-	bzero(buf, strlen(str) + 1);
-	fd = freopen(FPATH, "w", stdout);
-	ft_putstr(str);
-	fclose(fd);
-	fd = fopen(FPATH, "r");
-	fread(buf, strlen(str) + 1, 1, fd);
-	fclose(fd);
+	printf("...ft_putmem\n");
+	rand_str(str, 41);
+	bzero(buf, 42);
+	file = freopen(FPATH, "w", stdout);
+	ft_putmem(str, 42);
+	fclose(file);
+	file = fopen(FPATH, "r");
+	fread(buf, 42, 1, file);
+	fclose(file);
 	freopen("/dev/tty", "w", stdout);
 	printf("[%s]\n", buf);
 	assert(strcmp(buf, str) == 0);
 	remove(FPATH);
-	free(buf);
 }
--- includes/tests.h
+++ includes/tests.h
@@ -6,7 +6,7 @@
 /*   By: tpolonen <tpolonen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/30 18:49:43 by tpolonen          #+#    #+#             */
-/*   Updated: 2021/11/30 18:55:55 by tpolonen         ###   ########.fr       */
+/*   Updated: 2021/12/08 20:32:59 by tpolonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,11 @@
 # include <time.h>
 # include <ctype.h>
 # include <limits.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
 # ifdef LINUX
 #  include <bsd/string.h>
 #  include <bsd/stdlib.h>
@@ -106,4 +111,6 @@ void	test_lstadd(void);
 void	test_lstiter(void);
 void	test_lstmap(void);
 
+/* Custom functions */
+void	test_putmem(void);
 #endif
